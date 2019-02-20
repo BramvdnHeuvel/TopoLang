6a6afb04c61461d6a271e558fe541915041d60c4
Bram <bram.vdh1999@gmail.com>
2019-02-20 13:48:15 +0100

    Setting up basic framework for topo initialization

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+
+def initialize_program():
+    """Reads the input and handles basic errors like
+    - When a file name isn't provided;
+    - When the provided file name cannot be found;
+    """
+    args = sys.argv[1:]
+    # print(args)
+
+def interpret_file():
+    """Reads through the file and asks to interpret the file."""
+    pass
+
+if __name__ == "__main__":
+    initialize_program()
