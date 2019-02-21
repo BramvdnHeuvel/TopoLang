5d7aad651ec6dd2019c87de89ce1a40f378aebf9
Bram <bram.vdh1999@gmail.com>
2019-02-21 16:04:31 +0100

    Add first stage of reading the file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+from objects import test_levels
 
 def initialize_program():
     """Reads the input and handles basic errors like
@@ -6,11 +7,41 @@ def initialize_program():
     - When the provided file name cannot be found;
     """
     args = sys.argv[1:]
-    # print(args)
-
-def interpret_file():
+    print(args)
+    if len(args) == 0:
+        print('ERROR: No file provided! Please provide us with a file!')
+        # TODO: Write program info instead of giving an error.
+    
+    else:
+        data_strip = read_file(args[0])
+        print(data_strip)
+        print(test_levels(data_strip))
+    
+def read_file(file_name):
     """Reads through the file and asks to interpret the file."""
-    pass
+    long_string = []
+
+    comment = False
+    lin = 0
+
+    try:
+        for line in open(file_name):
+            lin += 1
+            col = 0
+            for char in line:
+                col += 1
+                if comment or char == '$':
+                    comment = True
+                    if char == '\n':
+                        comment = False
+                
+                elif char not in [' ','\n','\t']:
+                    long_string.append((char,lin,col))
+    except FileNotFoundError:
+        print('Oh no! This file doesn\'t exist! Or, I cannot find it, at least.')
+        # TODO: Write program that handles non-existent files.
+    
+    return long_string
 
 if __name__ == "__main__":
     initialize_program()
